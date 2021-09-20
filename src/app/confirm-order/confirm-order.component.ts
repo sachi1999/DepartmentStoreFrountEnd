5cd3091bd696bc7aaea4902257a6c388c27be5f0
Sachidananda g <sachidananda.g@pelatro.com>
2021-09-20 14:34:05 +0530

    Order review validation

--- src/app/confirm-order/confirm-order.component.ts
+++ src/app/confirm-order/confirm-order.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ShoppingCartService } from '@app/services/shoppingCart.service';
-import { first } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirm-order',
@@ -8,11 +8,30 @@ import { first } from 'rxjs/operators';
   styleUrls: ['./confirm-order.component.scss']
 })
 export class ConfirmOrderComponent implements OnInit {
-  constructor(private shoppingCartService: ShoppingCartService) { }
-  ngOnInit(): void {
-    this.shoppingCartService.getShoppingCart()
-      .pipe(first())
-      .subscribe();
+  errMsg: string = '';
+  form: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      fullName: ['', Validators.required],
+      email: ['', Validators.required],
+      address: ['', Validators.required],
+      city: ['', [Validators.required]],
+      state: ['', [Validators.required]],
+      zipCode: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  onSubmit() {
+    // console.log(this.form.value);
+    this.router.navigate(['/']);
+    return;
   }
 
 }
